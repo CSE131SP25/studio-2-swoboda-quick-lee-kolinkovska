a8f511b9846d44dec99b51b8cdb9acfa17e7dad0
emilyswoboda <emilyswoboda@localhost>
2025-01-29 15:11:44 -0600

    Went to 'while'

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,10 +1,25 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Ruin {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		// TODO Auto-generated method stub
-
+		double startAmount = 0.0;	
+		double winChance = 0.0;
+		double winLimit = 0.0;
+		double currentAmount = 0.0;
+	
+	System.out.print("Type your starting amount here:");
+	startAmount = in.nextDouble();
+	System.out.print("Enter Win Chance:");
+	winChance = in.nextDouble();
+	System.out.print("Enter your Win Limit:");
+	winLimit = in.nextDouble();
+		
+while (winLimit < currentAmount) {
 	}
 
 }
